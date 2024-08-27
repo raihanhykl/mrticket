531b0524d06adf09d283b6b0da50628de3672729
daffazuhdi <daffazhdihh@gmail.com>
2024-08-27 21:34:06 +0700

    Feat: Navbar V1 (Search belom)

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google';
 import './globals.css';
 import { Header } from '@/components/Header';
 import { Footer } from '@/components/Footer';
+import Navbar from '@/components/Navbar';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -19,9 +20,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Header />
-        {children}
-        <Footer />
+        <center>
+          <Navbar/>
+          {children}
+          <Footer />
+        </center>
       </body>
     </html>
   );
---
+++ apps/web/src/components/Navbar.tsx
@@ -0,0 +1,62 @@
+import Link from 'next/link';
+import React from 'react';
+
+type Props = {};
+
+export default function Navbar({}: Props) {
+  return <>
+      <div className="flex px-[30px] py-[10px] gap-5 bg-[#003899] text-white justify-end text-[12px]">
+        <a href="">Tentang Loket</a>
+        <a href="">Mulai Jadi Event Creator</a>
+        <a href="">Biaya</a>
+        <a href="">Blog</a>
+        <a href="">Hubungi Kami</a>
+        <a href="">Loket Screen</a>
+      </div>
+
+      <div className=" bg-[#152955] text-white px-[40px] py-[16px]  text-sm">
+        <div className="flex justify-between">
+            <Link href={"/"}>
+                <img src="https://assets.loket.com/images/logo-loket-white.png" alt="" className='h-8'/>
+            </Link>
+                
+
+            <div className="content-center">search</div>
+            
+            <div className="flex gap-5">
+                <div className="content-center flex items-center gap-1">
+                    <img src="https://assets.loket.com/web/assets/img/ic_schedule.svg" alt="" className='w-[21px]'/>
+                    <a href="">Buat Event</a>
+                </div>
+                <div className="content-center flex items-center gap-1">
+                    <img src="	https://assets.loket.com/web/assets/img/ic_explore_compass.svg" alt="" className='w-[21px]' />
+                    <a href="">Jelajah</a>
+                </div>
+                <div className="flex gap-2">
+                    <Link href={"/register"}> 
+                        <div className="content-center border-[1px] rounded-md p-2">
+                            <p>Daftar</p>
+                        </div>
+                    </Link>
+                    <Link href={"/login"}> 
+                        <div className="content-center border-[1px] rounded-md p-2">
+                            <p>Masuk</p>
+                        </div>
+                    </Link>
+                </div>
+            </div>
+
+        </div>
+        <div className="flex gap-4 justify-center bg-[#152955] text-white text-sm">
+            <Link href={""}>#LoketMart</Link>
+            <Link href={""}>#Promo_Indodana</Link>
+            <Link href={""}>#LOKETScreen</Link>
+            <Link href={""}>#LOKET_Promo</Link>
+            <Link href={""}>#motoGP</Link>
+            <Link href={""}>#LoketAttraction</Link>
+        </div>
+      </div>
+
+      
+    </>
+}
